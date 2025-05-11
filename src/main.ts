86c94815349f32df98a61263c28636b710207347
Victor Olorunfemi <victorayomide32@gmail.com>
2025-05-11 19:41:51 +0100

    moved logger file into main parent folder

--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,6 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
-import { LoggerFactory } from './common/factory/logger/logger.factory';
+import { LoggerFactory } from './common/factory/logger.factory';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule, {
